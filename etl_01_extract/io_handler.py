7b092eadc2611174c8bfd8ea79cf89a3c8cd72b5
Mark_Chen <qwertyuiop604913@gmail.com>
2025-12-05 15:36:08 +0800

    code 修改路徑

--- etl_01_extract/io_handler.py
+++ etl_01_extract/io_handler.py
@@ -3,47 +3,6 @@ from typing import Optional
 
 class IOHandler:
     def __init__(self, 
-                 input_path: Optional[str] = None, 
-                 output_path: Optional[str] = None,
-                 encoding: str =  'utf-8'):
-        self.input_path = input_path
-        self.output_path = output_path
-        self.encoding = encoding
-    def load(self):
-        if not self.input_path:
-            raise ValueError("未指定輸入路徑")
-        try:
-            df = pd.read_csv(self.input_path, encoding =self.encoding,  sep = ",")
-            print("使用utf-8編碼傳入")
-            return df
-            
-        except UnicodeDecodeError:
-            df = pd.read_csv(self.input_path, encoding = "big5", sep = "\t")
-            print("使用big5編碼傳入")
-            return df
-    def save(self, df):
-        if not self.output_path:
-            raise ValueError("未指定輸出路徑")
-        df.to_csv(self.output_path, encoding = "utf-8", index = False, sep = ",")
-        print(f"以utf-8儲存:{self.output_path}")
-
-def main():
-    io = IOHandler("input_data.csv", "output_data.csv")
-    df = io.load()
-    io.save(df)
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-import pandas as pd
-from typing import Optional
-
-class IOHandler:
-    def __init__(self, 
                  input_path : Optional[str] = None,
                  output_path : Optional[str] = None,
                  encoding : str = "utf-8"
@@ -79,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
